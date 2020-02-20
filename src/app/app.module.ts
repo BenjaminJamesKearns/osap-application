dd7425fd96c7e2cc15beae19c8964abb4db2942a
BenjaminJamesKearns <rapiidz1@gmail.com>
2020-02-20 13:13:06 +0000

    put header, footer and navigation box into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationBoxComponent } from './navigation-box/navigation-box.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent, NavigationBoxComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/navigation-box/navigation-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-box',
+  templateUrl: './navigation-box.component.html',
+  styleUrls: ['./navigation-box.component.css']
+})
+export class NavigationBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
